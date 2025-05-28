5e7f7a66b30a33eae56eedc79212d2fd8edf9602
Victor Oliveira <victor.rsoliveira@gmail.com>
2025-05-28 00:17:42 -0300

    example to test abortion of running node
    Signed-off-by: Victor Oliveira <victor.rsoliveira@gmail.com>

--- btreefy-examples.c
+++ btreefy-examples.c
@@ -2,12 +2,14 @@
 
 #include "btreefy.h"
 
-btf_node_status_t mock_run_ble_action_status = BTF_FAILURE_STATUS;
+btf_node_status_t mock_run_ble_action_status         = BTF_SUCCESS_STATUS;
+btf_node_status_t mock_run_lora_action_status        = BTF_RUNNING_STATUS;
+btf_node_status_t mock_reset_variables_action_status = BTF_SUCCESS_STATUS;
 
 btf_node_status_t reset_variables_action(btf_tree_st *tree, void *data,
                                          size_t datalen)
 {
-    return BTF_FAILURE_STATUS;
+    return mock_reset_variables_action_status;
 }
 
 btf_node_status_t run_ble_action(btf_tree_st *tree, void *data, size_t datalen)
@@ -17,7 +19,7 @@ btf_node_status_t run_ble_action(btf_tree_st *tree, void *data, size_t datalen)
 
 btf_node_status_t run_lora_action(btf_tree_st *tree, void *data, size_t datalen)
 {
-    return BTF_RUNNING_STATUS;
+    return mock_run_lora_action_status;
 }
 
 btf_node_status_t go_to_sleep_action(btf_tree_st *tree, void *data,
@@ -30,7 +32,7 @@ struct btf_node nodes[] = {
 
     [0] = {.status  = BTF_UNDEF_STATUS,
            .parent  = BTF_NULL_NODE,
-           .child   = 1,
+           .child   = 2,
            .sibling = BTF_NULL_NODE,
            .action  = NULL,
            .control = btf_sequence_policy_fn,
@@ -45,9 +47,9 @@ struct btf_node nodes[] = {
            .name    = "Success_1"},
 
     [2] = {.status  = BTF_UNDEF_STATUS,
-           .parent  = 1,
+           .parent  = 0,
            .child   = BTF_NULL_NODE,
-           .sibling = BTF_NULL_NODE,
+           .sibling = 3,
            .action  = reset_variables_action,
            .control = NULL,
            .name    = "Reset_Variables"},
@@ -57,8 +59,8 @@ struct btf_node nodes[] = {
            .child   = 4,
            .sibling = 6,
            .action  = NULL,
-           .control = btf_fallback_policy_fn,
-           .name    = "Fallback_1"},
+           .control = btf_sequence_policy_fn,
+           .name    = "Sequence_2"},
 
     [4] = {.status  = BTF_UNDEF_STATUS,
            .parent  = 3,
@@ -100,13 +102,29 @@ int main(void)
 
     if (btf_init(&tree, nodes, sizeof(nodes)) == 0)
     {
-        for (size_t i = 0; i < 2; i++)
+        for (size_t i = 0; i < 3; i++)
         {
             status = btf_tick_tree(&tree);
             printf("Tree executed and returned %s\n",
                    btf_global_action_status_string[status]);
-            mock_run_ble_action_status = BTF_SUCCESS_STATUS;
             printf("-------------------------------------------\n");
+            switch (i)
+            {
+            case 0:
+                //   mock_run_ble_action_status = BTF_FAILURE_STATUS;
+                mock_reset_variables_action_status = BTF_FAILURE_STATUS;
+                break;
+
+            case 1:
+                mock_run_lora_action_status        = BTF_SUCCESS_STATUS;
+                mock_reset_variables_action_status = BTF_SUCCESS_STATUS;
+                //   mock_run_ble_action_status = BTF_SUCCESS_STATUS;
+                break;
+
+            default:
+                //   mock_run_ble_action_status = BTF_SUCCESS_STATUS;
+                break;
+            }
         }
     }
     return 0;
