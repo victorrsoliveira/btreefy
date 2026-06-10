4afc0b69bfad1dd4690cd128bb9afecfbd3f551c
Victor Oliveira <victor.rsoliveira@gmail.com>
2026-06-10 01:42:08 -0300

    handle abort procedure and clean running node
    - call abort when a node returns RUNNING and there is another running
    node already
    - clean running node reference when it returns success or failure

--- src/btreefy.c
+++ src/btreefy.c
@@ -10,6 +10,7 @@
 #include "btreefy/btreefy.h"
 
 #include <stdio.h>
+
 #include "btreefy/btreefy_objs.h"
 
 // Global
@@ -23,6 +24,8 @@ char *btf_global_control_status_string[] = {
     [BTF_CONTINUE_EXECUTION_RESULT] = "CONTINUE",
     [BTF_RETURN_EXECUTION_RESULT]   = "RETURN",
     [BTF_PAUSE_EXECUTION_RESULT]    = "PAUSE"};
+
+static uintptr_t abort_running_node_ahead(struct btf_tree *tree);
 
 int32_t btf_init(struct btf_tree *tree, struct btf_node *nodes,
                  uint32_t tree_size)
@@ -97,6 +100,12 @@ int32_t btf_tick_tree(struct btf_tree *tree)
 
             if (BTF_RUNNING_STATUS == status)
             {
+                if ((tree->running_node_index != BTF_NULL_NODE)
+                    && (tree->running_node_index
+                        != BTF_NODE_ARRAY_INDEX(tree->nodes, p_node)))
+                {
+                    abort_running_node_ahead(tree);
+                }
                 // Store the running node index
                 tree->running_node_index =
                     BTF_NODE_ARRAY_INDEX(tree->nodes, p_node);
@@ -104,6 +113,20 @@ int32_t btf_tick_tree(struct btf_tree *tree)
                 // / (uintptr_t) sizeof(struct btf_node);
                 printf("Store RUNNING node [%u]\n",
                        (uint32_t) tree->running_node_index);
+            }
+            else if ((tree->running_node_index
+                      == BTF_NODE_ARRAY_INDEX(tree->nodes, p_node))
+                     && ((status == BTF_SUCCESS_STATUS)
+                         || (status == BTF_FAILURE_STATUS)))
+            {
+                // Node is no longer running since it has returned success or failure
+                printf("Node [%u] is no longer running since it has returned success or failure \n",
+                       (uint32_t) tree->running_node_index);
+                tree->running_node_index = BTF_NULL_NODE;
+            }
+            else
+            {
+                // TODO: Handle when staus is UNDEF
             }
         }
         else
@@ -157,4 +180,32 @@ int32_t btf_tree_controller(struct btf_tree *tree)
     }
 
     return 0;
+}
+
+static uintptr_t abort_running_node_ahead(struct btf_tree *tree)
+{
+    uintptr_t curr_running_node        = tree->running_node_index;
+    uintptr_t node_with_running_status = curr_running_node;
+
+    enum btf_node_status status = BTF_UNDEF_STATUS;
+
+    printf("Must abort RUNNING action [%lu]\n", curr_running_node);
+
+    // ? QUESTION: Should user indicate whether the abort signal was handled?
+    // ? If so, there should be a return value for that
+    status = tree->nodes[curr_running_node].action(tree, BTF_ABORT_SIGNAL);
+
+    // Clear all nodes' status from the running (aborted) node
+    // until current node being evaluated
+    while (
+        (node_with_running_status != BTF_NULL_NODE)
+        && (tree->nodes[node_with_running_status].status == BTF_RUNNING_STATUS))
+    {
+        printf("Cleaning RUNNING status on node at index %lu\n",
+               node_with_running_status);
+        tree->nodes[node_with_running_status].status = BTF_UNDEF_STATUS;
+        node_with_running_status = tree->nodes[node_with_running_status].parent;
+    }
+
+    return curr_running_node;
 }
